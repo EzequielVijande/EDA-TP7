4b6bcf6d288c76d7dc898d12f8f4e6b04f5ab4a0
ManuMollon261297 <mmollon@itba.edu.ar>
2017-10-16 18:43:02 -0300

    agrego la base de la parte de eventGenerator

--- Worms Networking/Worms Networking/EventGenerator.cpp
+++ Worms Networking/Worms Networking/EventGenerator.cpp
@@ -1,1 +1,123 @@
 #include "EventGenerator.h"
+
+EventGenerator::EventGenerator(Worm & worm, Graphic & graficos)
+{
+	quit = false;
+	for (int i = 0; i < 512; i++)
+	{
+		buffer[i] = 0;
+	}
+	//inicializar todos los datos miembros relacionados a worms y graphics;
+}
+
+EventGenerator::~EventGenerator()
+{
+}
+
+void EventGenerator::searchForEvents()
+{
+	bool finished = false;
+	int cant;
+	while (!finished)
+	{
+		if (al_get_next_event(eventQueue,&evento))
+		{
+			shape(evento);
+		}
+		else if (cant = socket_forServer->read_some(boost::asio::buffer(buffer)))
+		{
+			shape(buffer,cant);
+		}
+		else 
+		{
+			finished = true;
+		}
+	}
+}
+
+bool EventGenerator::hayEvento()
+{
+	if (eventList.size() == 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+
+bool EventGenerator::isNotQuit()
+{
+	return (!quit);
+}
+
+/*
+	La funcion  getNextEvent supone que hay por lo menos un elemento en la lista, se debe preguntar antes
+	si lo hay con la funcion hayEvento
+*/
+genericEvent EventGenerator::getNextEvent()
+{
+	std::list<genericEvent>::iterator it= eventList.begin();
+	genericEvent  current_ev = (*it);
+	eventList.pop_front();
+	return current_ev;
+}
+
+void EventGenerator::shape(ALLEGRO_EVENT ev)
+{
+		genericEvent events; //hay que cambiarlo a un wormEvent 
+		switch (ev.type) 
+		{
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				events.eventType = QUIT;
+				events.ud = 0;
+				quit = true;
+				break;
+			case ALLEGRO_EVENT_TIMER:
+				events.eventType = REFRESH;
+				events.ud = 0;
+				break;
+			case ALLEGRO_EVENT_KEY_DOWN:
+				switch (ev.keyboard.keycode)
+				{
+				case P1_LEFT:
+					events.eventType = PRESS_MOVE;
+					events.ud = P1_LEFT;
+					break;
+				case P1_RIGHT:
+					events.eventType = PRESS_MOVE;
+					events.ud = P1_RIGHT;
+					break;
+				case P1_UP:
+					events.eventType = PRESS_JUMP;
+					events.ud = P1_UP;
+					break;
+				}
+				break;
+			case ALLEGRO_EVENT_KEY_UP:
+				switch (ev.keyboard.keycode)
+				{
+				case P1_LEFT:
+					events.eventType = RELEASE_MOVE;
+					events.ud = P1_LEFT;
+					break;
+				case P1_RIGHT:
+					events.eventType = RELEASE_MOVE;
+					events.ud = P1_RIGHT;
+					break;
+				case P1_UP:
+					events.eventType = RELEASE_JUMP;
+					events.ud = P1_UP;
+					break;
+				}
+				break;
+		}
+		eventList.push_back(events);
+}
+
+void EventGenerator::shape(char * buf, unsigned int cant)
+{
+	//segun como sea cada package, hay que generar un evento correspondiente para despues meterlo en la cola de eventos
+}
--- Worms Networking/Worms Networking/EventGenerator.h
+++ Worms Networking/Worms Networking/EventGenerator.h
@@ -1,19 +1,53 @@
-#ifndef EVENT_GENERATOR_H
-#define EVENT_GENERATOR_H
+#pragma once
 
-#include "Event.h"
 #include <list>
+#include "Worm.h"
+#include "allegro5\allegro.h"
+#include "boost\asio.hpp"
+
+//#include "genericEvent.h"
+//#include "Graphics.h"
+
+enum event{PRESS_MOVE, RELEASE_MOVE, PRESS_JUMP, RELEASE_JUMP, REFRESH, QUIT, NO_EVENT};
+
+#define P1_LEFT		ALLEGRO_KEY_LEFT
+#define P1_RIGHT	ALLEGRO_KEY_RIGHT
+#define P1_UP		ALLEGRO_KEY_UP
+
+class genericEvent
+{
+public:
+	int eventType;
+	char ud;
+
+};
+class Graphic
+{
+	int hay_que_agregarlo;
+};
 
 class EventGenerator
 {
 public:
-	EventGenerator();
+	EventGenerator(Worm& worm, Graphic& graficos);
 	~EventGenerator();
-	void SearchForEvents();
-
+	void searchForEvents();
+	bool hayEvento();
+	bool isNotQuit();
+	genericEvent getNextEvent();
 private:
-	std::list<GenericEvent> EventList;
+	void shape(ALLEGRO_EVENT ev);
+	void shape(char * buf, unsigned int cant);
 
+	ALLEGRO_EVENT_QUEUE * eventQueue;
+	ALLEGRO_EVENT evento;
+
+	char buffer[512];
+	bool quit;
+	std::list<genericEvent> eventList;
+
+	boost::asio::io_service*  IO_handler;
+	boost::asio::ip::tcp::socket* socket_forServer;
+	boost::asio::ip::tcp::acceptor* server_acceptor;
 };
 
-#endif //EVENT_GENERATOR_H
