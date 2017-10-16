ee1f8937e51a2de6c3251157103eeec21b1d5494
Ezequiel Vijande <evijande@itba.edu.ar>
2017-10-16 13:46:56 -0300

    Meti la idea general de la clase padre para los estados

--- Worms Networking/Worms Networking/FSM.cpp
+++ Worms Networking/Worms Networking/FSM.cpp
@@ -9,23 +9,23 @@ void fsm:: dispatch(GenericEvent* ev)
 	switch (ev->GetEventValue())
 	{
 		case EV0:
-			prox_estado = estado->RunRoutine((ev0*)ev);
+			prox_estado = estado->OnEvent0((ev0*)ev);
 			break;
 
 		case EV1:
-			prox_estado = estado->RunRoutine((ev1*)ev);
+			prox_estado = estado->OnEvent1((ev1*)ev);
 			break;
 
 		case EV2:
-			prox_estado = estado->RunRoutine((ev2*)ev);
+			prox_estado = estado->OnEvent2((ev2*)ev);
 			break;
 
 		case EV3:
-			prox_estado = estado->RunRoutine((ev3*)ev);
+			prox_estado = estado->OnEvent3((ev3*)ev);
 			break;
 
 		case EV4:
-			prox_estado = estado->RunRoutine((ev4*)ev);
+			prox_estado = estado->OnEvent4((ev4*)ev);
 			break;
 	}
 	if (prox_estado != nullptr)
--- Worms Networking/Worms Networking/State.h
+++ Worms Networking/Worms Networking/State.h
@@ -1,13 +1,34 @@
 #ifndef STATE_H
 #define STATE_H
 
+enum State{IDLE, W_WAIT, J_WAIT, WALKING, JUMPING, LISTENING};
 
 class GenericState
 {
 public:
 	GenericState();
 	virtual ~GenericState();
-	void virtual RunRoutine(GenericEvent*)=0;
+	virtual GenericState* OnEvent0(ev0*)
+	{
+		return this;
+	}
+	virtual GenericState* OnEvent1(ev1*)
+	{
+		return this;
+	}
+	virtual GenericState* OnEvent2(ev2*)
+	{
+		return this;
+	}
+	virtual GenericState* OnEvent3(ev3*)
+	{
+		return this;
+	}
+	virtual	GenericState* OnEvent4(ev4*)
+	{
+		return this;
+	}
+
 };
 
 #endif //STATE_H
