fc0b885d66d459894d23ae111e393f6cd57aaf78
Ezequiel Vijande <evijande@itba.edu.ar>
2017-10-17 21:44:28 -0300

    Updatie los headers de los eventos derivados

--- Worms Networking/Worms Networking/RefreshEvent.h
+++ Worms Networking/Worms Networking/RefreshEvent.h
@@ -1,12 +1,20 @@
 #ifndef REFRESH_EVENT
 #define REFRESH_EVENT
 #include "Event.h"
+#include "Output.h"
 
 class RefreshEvent : public GenericEvent
 {
 private:
+	Graphic* viewer;
 
+	//Datos de Boost
+	char buf[512];
+	char sequence[255];
 
+	boost::asio::io_service*  IO_handler;
+	boost::asio::ip::tcp::socket* socket_forServer;
+	boost::asio::ip::tcp::acceptor* server_acceptor;
 };
 
 #endif //REFRESH_EVENT
--- Worms Networking/Worms Networking/WormEvent.h
+++ Worms Networking/Worms Networking/WormEvent.h
@@ -1,10 +1,12 @@
 #ifndef WORM_EVENT
 #define WORM_EVENT
 #include "Event.h"
-
+#include "Worm.h"
 class WormEvent : public GenericEvent
 {
 
+private:
+	Worm* worm;
 };
 
 
